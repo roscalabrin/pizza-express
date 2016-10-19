efb7d5fdaa9330d52338e343f96b9c92b974db0e
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-19 11:44:21 -0600

    add request tests

--- test/server-test.js
+++ test/server-test.js
@@ -9,6 +9,9 @@ describe('Server', () => {
       if (err) {return done(err)}
       done()
     })
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876'
+    })
   })
 
   after(() => {
@@ -23,8 +26,20 @@ describe('Server', () => {
 
   describe('GET /', () => {
     it('should return a 200', (done) => {
-      request.get('http://localhost:9876', (error, response) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error) }
         assert.equal(response.statusCode, 200)
+        done()
+      })
+    })
+
+    it('should have a body with the name of the application', (done) => {
+      const title = app.locals.title
+
+      this.request.get('/', (error, response) => {
+        if (error) { done(error) }
+        assert(response.body.includes(title),
+          `"${response.body}" does not include "${title}"`)
         done()
       })
     })
