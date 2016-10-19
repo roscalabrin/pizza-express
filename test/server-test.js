2014d8794cd50768f044aadc8fb65c5724c0206a
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-19 11:05:58 -0600

    add test for endpoint

---
+++ test/server-test.js
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const app = require('../server')
+const request = require('request')
+
+describe('Server', () => {
+  before(done => {
+    this.port = 9876
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) {return done(err)}
+      done()
+    })
+  })
+
+  after(() => {
+    this.server.close()
+  })
+
+  describe('Server', () => {
+    it('should exist', () => {
+      assert(app)
+    })
+  })
+
+  describe('GET /', () => {
+    it('should return a 200', (done) => {
+      request.get('http://localhost:9876', (error, response) => {
+        assert.equal(response.statusCode, 200)
+        done()
+      })
+    })
+  }) 
+})
