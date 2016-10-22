a7a5bf08b8bacfe4b42da73ba1138f84bc2be6c2
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-22 14:43:36 -0600

    refactor

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
+
 const path = require('path')
+const bodyParser = require('body-parser')
+
+const generateId = require('./lib/generate-id')
 
 app.use(express.static('static'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'jade')
+
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Pizza Express'
+app.locals.pizzas = {}
 
 app.get('/', (request, response) => {
-  // response.sendFile(path.join(_dirname, '/static/index.html'))
   response.render('index')
+})
+
+app.post('/pizzas', (request, response) => {
+  var id = generateId()
+  app.locals.pizzas[id] = request.body
+  response.sendStatus(201)
 })
 
 if (!module.parent) {
--- test/server-test.js
+++ test/server-test.js
@@ -1,14 +1,17 @@
 const assert = require('assert')
 const app = require('../server')
 const request = require('request')
+const fixtures = require('./fixtures')
 
 describe('Server', () => {
+
   before((done) => {
     this.port = 9876
     this.server = app.listen(this.port, (err, result) => {
       if (err) {return done(err)}
       done()
     })
+
     this.request = request.defaults({
       baseUrl: 'http://localhost:9876'
     })
@@ -44,4 +47,35 @@ describe('Server', () => {
       })
     })
   }) 
+
+  describe('POST /pizzas', () => {
+    beforeEach(() => {
+      app.locals.pizzas = {}
+    })
+
+    it('should not return 404', (done) => {
+      this.request.post('/pizzas', (error, response) => {
+        if (error) { done(error) }
+        assert.notEqual(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('should receive and store data', (done) => {
+      var validPizza = {
+        pizza: {
+          name: 'A vegan pizza',
+          toppings: ['mushrooms', 'onions', 'garlic', 'black olives' ]
+        }
+      }
+
+      this.request.post('/pizzas', { form: validPizza }, (error, response) => {
+        if (error) { done(error) }
+
+        var pizzaCount = Object.keys(app.locals.pizzas).length
+        assert.equal(pizzaCount, 1, `Expect 1 pizzas, found ${pizzaCount}`) 
+        done()
+      })
+    })
+  })
 })
